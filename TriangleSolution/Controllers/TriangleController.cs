a6c24cd2ca66162ee92713c048ac0483aa4a3e84
mpaliichuk <marian.paliichuk@gmail.com>
2023-09-12 17:22:17 +0300

    first commit. All methods are created. Some tests are failing.

---
+++ TriangleSolution/Controllers/TriangleController.cs
@@ -0,0 +1,213 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Triangles.Models;
+
+namespace Triangles.Controllers
+{
+    public class TriangleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Triangle");
+        }
+        [HttpGet("triangle/perimeter")]
+        public IActionResult Perimeter(double side1, double side2, double side3)
+        {
+            if (!Triangle.IsValid(side1, side2, side3))
+            {
+                return BadRequest("Invalid triangle sides. They cannot form a triangle.");
+            }
+            var triangle = new Triangle(side1, side2, side3);
+
+            double perimeter = triangle.CalculatePerimeter();
+
+            return Content(perimeter.ToString("F2"));
+        }
+
+        [HttpGet("triangle/isrightangled")]
+        public IActionResult IsRightAngled(double side1, double side2, double side3)
+        {
+            if (!Triangle.IsValid(side1, side2, side3))
+            {
+                return BadRequest("Invalid triangle sides. They cannot form a triangle.");
+            }
+            var triangle = new Triangle(side1, side2, side3);
+
+            bool isRightAngled = triangle.IsRightAngled();
+
+            return Content(isRightAngled.ToString());
+        }
+
+        [HttpGet("triangle/isequilateral")]
+        public IActionResult IsEquilateral(double side1, double side2, double side3)
+        {
+            var triangle = new Triangle(side1, side2, side3);
+            var isEquilateral = triangle.IsEquilateral(triangle);
+            return Content(isEquilateral.ToString());
+        }
+
+        [HttpGet("triangle/isisosceles")]
+        public IActionResult IsIsosceles(double side1, double side2, double side3)
+        {
+            var triangle = new Triangle(side1, side2, side3);
+            var isIsosceles = triangle.IsIsosceles();
+            return Content(isIsosceles.ToString());
+        }
+
+        
+        public bool AreCongruent(Triangle tr1, Triangle tr2)
+        {
+            bool areCongruent = Triangle.AreCongruent(tr1, tr2);
+            return areCongruent;
+        }
+
+        [HttpGet("triangle/aresimilar")]
+        public IActionResult AreSimilar(double side1, double side2, double side3, double side4, double side5, double side6)
+        {
+            var triangle1 = new Triangle(side1, side2, side3);
+            var triangle2 = new Triangle(side4, side5, side6);
+            bool areSimilar = Triangle.AreSimilar(triangle1, triangle2);
+            return Content(areSimilar.ToString().ToLower());
+        }
+
+        [HttpGet("triangle/infogreatestperimeter")]
+        public string InfoGreatestPerimeter(Triangle[] tr)
+        {
+            //if (tr == null || tr.Length == 0)
+            //{
+            //    return BadRequest("No triangles provided.");
+            //}
+            Triangle greatestPerimeterTriangle = Triangle.FindTriangleWithGreatestPerimeter(tr);
+
+            //if (greatestPerimeterTriangle == null)
+            //{
+            //    return BadRequest("Invalid triangles provided.");
+            //}
+
+            double area = greatestPerimeterTriangle.CalculateArea();
+            double perimeter = greatestPerimeterTriangle.CalculatePerimeter();
+
+            string result = $"Triangle:\n({greatestPerimeterTriangle.Side2}, {greatestPerimeterTriangle.Side1}, {greatestPerimeterTriangle.Side3})\n";
+            result += $"Reduced:\n({greatestPerimeterTriangle.Side2 / perimeter:F2}, {greatestPerimeterTriangle.Side1 / perimeter:F2}, {greatestPerimeterTriangle.Side3 / perimeter:F2})\n";
+            result += $"\nArea = {area:F2}\n";
+            result += $"Perimeter = {perimeter}";
+
+            return result;
+        }
+
+        [HttpGet("triangle/numberspairwisenotsimilar")]
+        public string NumbersPairwiseNotSimilar(Triangle[] tr)
+        {
+            //if (tr == null || tr.Length < 2)
+            //{
+            //    return BadRequest("At least two triangles are required.");
+            //}
+
+            List<string> nonSimilarPairs = new List<string>();
+
+            for (int i = 0; i < tr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < tr.Length; j++)
+                {
+                    if (!Triangle.AreSimilar(tr[i], tr[j]))
+                    {
+                        nonSimilarPairs.Add($"({i + 1}, {j + 1})");
+                    }
+                }
+            }
+
+            string result = string.Join(Environment.NewLine, nonSimilarPairs);
+
+            return result;
+        }
+        [HttpGet("triangle/infogreatestarea")]
+        public string InfoGreatestArea(Triangle[] tr)
+        {
+            if (tr == null || tr.Length == 0)
+            {
+                //return BadRequest("No triangles provided.");
+            }
+
+            Triangle greatestAreaTriangle = tr.OrderByDescending(t => t.CalculateArea()).First();
+
+            double area = greatestAreaTriangle.CalculateArea();
+            double perimeter = greatestAreaTriangle.CalculatePerimeter();
+
+            string result = $"Triangle:\n({greatestAreaTriangle.Side3:F2}, {greatestAreaTriangle.Side2}, {greatestAreaTriangle.Side1})\n";
+            result += $"Reduced:\n({greatestAreaTriangle.Side1 / perimeter:F2}, {greatestAreaTriangle.Side2 / perimeter:F2}, {greatestAreaTriangle.Side3 / perimeter:F2})\n";
+            result += $"\nArea = {area:F2}\n";
+            result += $"Perimeter = {perimeter:F2}";
+
+            return result;
+        }
+
+        public object Info(Triangle triangle)
+        {
+            double[] sides = { triangle.Side1, triangle.Side2, triangle.Side3 };
+            Array.Sort(sides);
+
+            var triangle1 = new Triangle(sides[0], sides[1], sides[2]);
+            var area = triangle1.CalculateArea();
+            var perimeter = triangle1.CalculatePerimeter();
+
+            string info = $"Triangle:\n({sides[0]}, {sides[1]}, {sides[2]})\n";
+            info += $"Reduced:\n({sides[0] / perimeter:F2}, {sides[1] / perimeter:F2}, {sides[2] / perimeter:F2})\n";
+            info += $"\nArea = {area:F2}\n";
+            info += $"Perimeter = {perimeter}";
+
+            return info;
+        }
+
+        [HttpGet("triangle/area")]
+        public string Area(Triangle triangle)
+        {
+
+            var triangle1 = new Triangle(triangle.Side1, triangle.Side2, triangle.Side3);
+            double area = triangle.CalculateArea();
+
+            return area.ToString("F4");
+        }
+
+        public object Perimeter(Triangle triangle)
+        {
+            var triangle1 = new Triangle(triangle.Side1, triangle.Side2, triangle.Side3);
+
+            double perimeter = triangle.CalculatePerimeter();
+
+            return perimeter.ToString();
+        }
+
+        public bool IsRightAngled(Triangle triangle)
+        {
+            var triangle1 = new Triangle(triangle.Side1, triangle.Side2, triangle.Side3);
+
+            bool isRightAngled = triangle.IsRightAngled();
+
+            return isRightAngled;
+        }
+
+        public bool IsEquilateral(Triangle triangle)
+        {
+            var triangle1 = new Triangle(triangle.Side1, triangle.Side2, triangle.Side3);
+            var isEquilateral = triangle.IsEquilateral(triangle1);
+            return isEquilateral;
+        }
+
+        public bool IsIsosceles(Triangle triangle)
+        {
+            var triangle1 = new Triangle(triangle.Side1, triangle.Side2, triangle.Side3);
+            var isIsosceles = triangle.IsIsosceles();
+            return isIsosceles;
+        }
+
+        public object AreSimilar(Triangle triangle1, Triangle triangle2)
+        {
+            var triangle_1 = new Triangle(triangle1.Side1, triangle1.Side2, triangle1.Side3);
+            var triangle_2 = new Triangle(triangle2.Side1, triangle2.Side2, triangle2.Side3);
+            bool areSimilar = Triangle.AreSimilar(triangle_1, triangle_2);
+            return areSimilar;
+        }
+    }
+}
---
+++ TriangleSolution/Models/Triangle.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Linq;
+
+namespace Triangles.Models
+{
+    public class Triangle
+    {
+        private double side1;
+        private double side2;
+        private double side3;
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;           
+        }
+        public Triangle()
+        {
+            
+        }
+
+        public double Side1 
+        {
+            get { return side1; } 
+            set { side1 = value; }
+        }
+        public double Side2
+        {
+            get { return side2; }
+            set { side2 = value; }
+        }
+        public double Side3
+        {
+            get { return side3; }
+            set { side3 = value; }
+        }
+        public double CalculateArea()
+        {
+            double s = (Side1 + Side2 + Side3) / 2;
+            return Math.Sqrt(s * (s - Side1) * (s - Side2) * (s - Side3));
+        }
+        public double CalculatePerimeter()
+        {
+            return Side1 + Side2 + Side3;
+        }
+
+        public double CalculateReducedSide1()
+        {
+            return Side1 - (Side2 + Side3);
+        }
+
+        public double CalculateReducedSide2()
+        {
+            return Side2 - (Side1 + Side3);
+        }
+
+        public double CalculateReducedSide3()
+        {
+            return Side3 - (Side1 + Side2);
+        }
+        public bool IsRightAngled()
+        {
+            double[] sides = { Side1, Side2, Side3 };
+            Array.Sort(sides);
+
+            double a = sides[0];
+            double b = sides[1];
+            double c = sides[2];
+
+            double tolerance = 0.0001;
+
+            return Math.Abs(a * a + b * b - c * c) < tolerance;
+        }
+        public bool IsEquilateral(Triangle triangle)
+        {
+            double tolerance = 0.001;
+
+            double[] sides = { triangle.Side1, triangle.Side2, triangle.Side3 };
+
+            if (Math.Abs(sides[0] - sides[1]) > tolerance ||
+                Math.Abs(sides[0] - sides[2]) > tolerance ||
+                Math.Abs(sides[1] - sides[2]) > tolerance)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public bool IsIsosceles()
+        {
+            double tolerance = 0.001;
+
+            double[] sides = { Side1, Side2, Side3 };
+            Array.Sort(sides);
+
+            double shortestSide = sides[0];
+            double middleSide = sides[1];
+            double longestSide = sides[2];
+
+
+
+            return Math.Abs(shortestSide - middleSide) < tolerance ||
+                   Math.Abs(middleSide - longestSide) < tolerance;
+        }
+        public static bool AreCongruent(Triangle tr1, Triangle tr2)
+        {
+            double tolerance = 0.001;
+            double ratio1 = tr1.Side1 / tr2.Side1;
+            double ratio2 = tr1.Side2 / tr2.Side2;
+            double ratio3 = tr1.Side3 / tr2.Side3;
+
+            bool sidesCongruent = Math.Abs(ratio1 - ratio2) < tolerance
+                                  && Math.Abs(ratio1 - ratio3) < tolerance
+                                  && Math.Abs(ratio2 - ratio3) < tolerance;
+
+            return sidesCongruent;
+        }
+
+        public static bool AreSimilar(Triangle triangle1, Triangle triangle2)
+        {
+            double ratio1 = triangle1.Side1 / triangle2.Side1;
+            double ratio2 = triangle1.Side2 / triangle2.Side2;
+            double ratio3 = triangle1.Side3 / triangle2.Side3;
+
+            double tolerance = 0.001;
+            bool areSimilar = Math.Abs(ratio1 - ratio2) < tolerance &&
+                              Math.Abs(ratio1 - ratio3) < tolerance &&
+                              Math.Abs(ratio2 - ratio3) < tolerance;
+
+            return areSimilar;
+        }
+        public static Triangle FindTriangleWithGreatestPerimeter(Triangle[] triangles)
+        {
+            if (triangles == null || triangles.Length == 0)
+            {
+                return null;
+            }
+
+            Triangle greatestPerimeterTriangle = triangles[0];
+            double greatestPerimeter = greatestPerimeterTriangle.CalculatePerimeter();
+
+            foreach (var triangle in triangles)
+            {
+                double perimeter = triangle.CalculatePerimeter();
+                if (perimeter > greatestPerimeter)
+                {
+                    greatestPerimeter = perimeter;
+                    greatestPerimeterTriangle = triangle;
+                }
+            }
+
+            return greatestPerimeterTriangle;
+        }
+        public bool AreSimilar(Triangle other)
+        {
+            double tolerance = 0.001;
+            double ratio1 = Side1 / other.Side1;
+            double ratio2 = Side2 / other.Side2;
+            double ratio3 = Side3 / other.Side3;
+
+            return Math.Abs(ratio1 - ratio2) < tolerance &&
+                   Math.Abs(ratio1 - ratio3) < tolerance &&
+                   Math.Abs(ratio2 - ratio3) < tolerance;
+        }
+        public static bool IsValid(double side1, double side2, double side3)
+        {
+            return side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1;
+        }
+        public static implicit operator double[](Triangle tr)
+        {
+            return new double[] {tr.side1,tr.side2,tr.side3 };
+        }
+    }
+}
